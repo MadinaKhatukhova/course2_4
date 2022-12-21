7a39c89e1d2a2acef733e200bba4651e41bfd724
Madina <madina-khat@yandex.ru>
2022-12-21 15:28:10 +0300

    ДЗ по OOP - polymorphism

---
+++ src/Truck.java
@@ -0,0 +1,33 @@
+public class Truck extends Transport implements Competing{
+
+
+    public Truck(String brand, String model, float engineVolume) {
+        super(brand, model, engineVolume);
+    }
+
+    @Override
+    public void go() {
+        System.out.println("Погнали!");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Стой-стой!");
+    }
+
+
+    @Override
+    public void pitStop() {
+        System.out.println("Пит-стоп");
+    }
+
+    @Override
+    public void bestLapTime() {
+        System.out.println("Лучшее время круга");
+    }
+
+    @Override
+    public void maxSpeed() {
+        System.out.println("Максимальная скорость");
+    }
+}
